dc9601a16e6e5a59644655d102d437a8bbce853c
kaunghtethtun <kaunghtethtun2013@gmail.com>
2024-08-15 01:18:44 +0630

    readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Learning
+Some Commands for making static library and link to main file 
+'''
+c++ -std=c++17 -c test.cpp -o test.o
+ar rcs libtest.a test.o ....
+c++ -std=c++17 main.cpp -L . ltest -o main
+'''
--- cmake/main.cpp
+++ cmake/main.cpp
@@ -1,9 +1,13 @@
 #include "test.hpp"
-
+#include "hi.hpp"
+#include <iostream>
 int main()
 {
+	a = 40;
+	b = 1;
 	Display();
 	Error();
-
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
 	return 0;
 }
